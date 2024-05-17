24aca5c390a320312b100f769075a50dbe66d41b
liebertmotion <137779989+Jonthedruger@users.noreply.github.com>
2024-05-17 13:08:07 +0700

    Create coins.lua

---
+++ coins.lua
@@ -0,0 +1,90 @@
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
+while wait(.01) do
+   game:GetService("ReplicatedStorage")[utf8.char(20195, 30721, 50, 46, 48)][utf8.char(25277, 22870)][utf8.char(38598, 32654, 20204, 24320, 25277, 37329, 24065)]:InvokeServer()
+end
